6d28344550ada5454a443958053831d316fa84d4
keithdang <keith.dang11@gmail.com>
2018-02-17 13:33:05 -0500

    Created a program that determine the number of levels in a tree and is it balanced

--- balancedTree.cpp
+++ balancedTree.cpp
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <vector>
 #include <unordered_set>
+#include <math.h>
+#include <cmath>
 #include "binaryTree.h"
 using namespace std;
 void balancedTree::start()
@@ -12,6 +14,7 @@ void balancedTree::start()
 	cout << "Balanced Tree\n";
 	node *bal = NULL;
 	createBalTree(&bal);
+	//createTree(&bal, 8, 9);
 	cout << "\nInorder Balanced\n";
 	printInorder(&bal);
 
@@ -19,10 +22,60 @@ void balancedTree::start()
 	{
 		cout << "\nThis is a perfect tree\n";
 	}
+	bool bIsBalanced = true;
+	int num = nLevels(&bal, bIsBalanced);
+	cout <<"\nTree has "<<num << " levels\n";
+	if (bIsBalanced)
+	{
+		cout << "Tree is balanced\n";
+	}
+	else
+	{
+		cout << "Tree is not balanced\n";
+	}
 	/*node *root = NULL;
 	createTree(&root, 10, 10);
 	cout << "Inorder Unbalanced\n";
 	printInorder(&root);*/
+}
+int balancedTree::nLevels(struct node **ref, bool& bIsBalanced)
+{
+	int levels = 0;
+	int leftCount = 0;
+	int rightCount = 0;
+	if ((*ref) != NULL)
+	{
+		levels = 1;
+		if ((*ref)->left != NULL)
+		{
+			leftCount = nLevels(&(*ref)->left, bIsBalanced);
+		}
+		if ((*ref)->right != NULL)
+		{
+			rightCount = nLevels(&(*ref)->right, bIsBalanced);
+		}
+		int nMax = maxNum(leftCount, rightCount);
+		if (abs(leftCount - rightCount) >= 2)
+		{
+			bBalanced = false;
+			bIsBalanced = false;
+		}
+		levels += nMax;
+	}
+	return levels;
+}
+int balancedTree::maxNum(int n, int m)
+{
+	int num=0;
+	if (m > n)
+	{
+		num = m;
+	}
+	else
+	{
+		num = n;
+	}
+	return num;
 }
 bool balancedTree::checkIfBalanced(struct node **ref)
 {
--- balancedTree.h
+++ balancedTree.h
@@ -6,7 +6,11 @@ public:
 	void start();
 	bool checkIfBalanced(struct node **ref);
 	bool isPerfectTree(struct node **ref);
+	int nLevels(struct node **ref, bool& bIsBalanced);
+	int maxNum(int n, int m);
 	balancedTree();
 	~balancedTree();
+private:
+	bool bBalanced = true;
 };
 
